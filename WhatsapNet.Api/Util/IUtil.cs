098834b1c266488cfbdbd6c11c35426e4fe1d362
livingstone23 <livingstone23@gmail.com>
2023-03-15 21:07:30 +0100

    Updating Util Class

--- WhatsapNet.Api/Util/IUtil.cs
+++ WhatsapNet.Api/Util/IUtil.cs
@@ -2,5 +2,15 @@
 {
     public interface IUtil
     {
+
+        object TextMessage(string message, string number);
+        object ImageMessage(string url, string number);
+        object AudioMessage(string url, string number);
+        object VideoMessage(string url, string number);
+        object DocumentMessage(string url, string number);
+
+        object LocationMessage(string number);
+        object ButtonMessage(string number);
+
     }
 }
--- WhatsapNet.Api/Util/Util.cs
+++ WhatsapNet.Api/Util/Util.cs
@@ -1,6 +1,144 @@
 ﻿namespace WhatsapNet.Api.Util
 {
-    public class Util
+    public class Util: IUtil
     {
+        public object TextMessage(string message, string number)
+        {
+            return new
+            {
+                messaging_product = "whatsapp",
+                to = number,
+                type = "text",
+                text = new
+                {
+                    body = message
+                }
+            };
+        }
+
+
+        public object ImageMessage(string url, string number)
+        {
+            return new
+            {
+                messaging_product = "whatsapp",
+                to = number,
+                type = "image",
+                image = new
+                {
+                    link = url
+                }
+            };
+        }
+
+
+        public object AudioMessage(string url, string number)
+        {
+            return new
+            {
+                messaging_product = "whatsapp",
+                to = number,
+                type = "audio",
+                audio = new
+                {
+                    link = url
+                }
+            };
+        }
+
+
+        public object VideoMessage(string url, string number)
+        {
+            return new
+            {
+                messaging_product = "whatsapp",
+                to = number,
+                type = "video",
+                video = new
+                {
+                    link = url
+                }
+            };
+        }
+
+
+        public object DocumentMessage(string url, string number)
+        {
+            return new
+            {
+                messaging_product = "whatsapp",
+                to = number,
+                type = "document",
+                document = new
+                {
+                    link = url
+                }
+            };
+        }
+
+
+        public object LocationMessage(string number)
+        {
+            return new
+            {
+                messaging_product = "whatsapp",
+                to = number,
+                type = "location",
+                location = new
+                {
+                    latitud = "40.449727",
+                    longitude = "-3.623424",
+                    name = "Provimad",
+                    address = "C. de Miguel Yuste, 18, Planta 4ª oficina 1, 28037 Madrid"
+                }
+            };
+        }
+
+
+        public object ButtonMessage(string number)
+        {
+            return new
+            {
+                messaging_product = "whatsapp",
+                to = number,
+                type = "interactive",
+                interactive = new
+                {
+                    type ="button",
+                    body = new
+                    {
+                        text = "Selecciona una opción"
+                    },
+                    action = new
+                    {
+                        buttons = new List<object>
+                        {
+                            new
+                            {
+                                type = "reply",
+                                reply = new
+                                {
+                                    id = "01",
+                                    title = "Comprar"
+                                }
+                            },
+                            new
+                            {
+                                type = "reply",
+                                reply = new
+                                {
+                                    id = "02",
+                                    title = "Vender"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+
     }
+
+
 }
